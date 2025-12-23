ef22265d0076afd3404922cf75c744f9c34d02d6
Bollineni <2463679@cognizant.com>
2025-12-23 13:05:57 +0530

    forget password created

--- ui/frontend/src/app/app.routes.ts
+++ ui/frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
+import { ForgotPassword } from './auth/forgot-password/forgot-password';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'signup',
         component: Signup,
+    },
+    {
+        path: 'forgot-password',
+        component: ForgotPassword,
     },
 ];
