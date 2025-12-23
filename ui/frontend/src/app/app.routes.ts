36300381e9bd6720262732ff394699daa1a39da1
Salunkhe <2463678@cognizant.com>
2025-12-23 17:26:25 +0530

    LANDING PAGE V1

--- ui/frontend/src/app/app.routes.ts
+++ ui/frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { LandingPage } from './pages/landing-page/landing-page';
+import { Landingpage } from './pages/landing-page/landing-page';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
 
 export const routes: Routes = [
     {
         path: '',
-        component: LandingPage,
+        component: Landingpage,
     },
     {
         path: 'login',
