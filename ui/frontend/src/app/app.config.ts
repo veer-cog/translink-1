a4342b697eb2ebe2c184447b144005241fe508a2
veer-cog <veersamirbha.patwa@cognizant.com>
2025-12-24 09:12:30 +0530

    signup added

--- ui/frontend/src/app/app.config.ts
+++ ui/frontend/src/app/app.config.ts
@@ -2,24 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-<<<<<<< HEAD
 import { routes } from './app.routes';
-=======
 import { MessageService } from 'primeng/api';
 
->>>>>>> d7ec342e1081a2712d1e818930c7da382b0cd0d3
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     
     providePrimeNG({
-<<<<<<< HEAD
-      theme: {
-        preset: Aura
-      }
-    })
-=======
             theme: {
                 preset: Aura,
                 options: {
@@ -27,6 +18,5 @@ export const appConfig: ApplicationConfig = {
           }
             }
         }),MessageService,
->>>>>>> d7ec342e1081a2712d1e818930c7da382b0cd0d3
   ]
 };
