f40de18cdd9f42b8a079cdce44d17d295bab59b8
veer-cog <veersamirbha.patwa@cognizant.com>
2026-01-09 17:37:54 +0530

    Signup Pushed

--- ui/frontend/src/app/app.routes.ts
+++ ui/frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Signup } from './auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'signup',component:Signup},
+    
+];
---
+++ ui/frontend/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthService } from '.././auth.service';
+
+// PrimeNG Imports
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageModule } from 'primeng/message';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputOtpModule } from 'primeng/inputotp';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss'],
+    standalone: true,
+    imports: [
+      CommonModule, FormsModule, ReactiveFormsModule, RouterLink,
+      InputTextModule, ButtonModule, ToastModule, MessageModule, 
+      CardModule, PasswordModule, FloatLabelModule, InputOtpModule
+    ],
+    providers: [MessageService] // Required for Toast
+})
+export class Signup {
+    // Inject the service as public so the HTML can access it directly
+    public auth = inject(AuthService);
+}
